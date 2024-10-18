e587458754bfa06acd32a9a21a47b52de1769b17
trebman95 <trebman91@hotmail.com>
2024-10-17 20:36:11 -0400

    first commit

---
+++ Tree.java
@@ -0,0 +1,8 @@
+public class Tree {
+    public static void main(String[] args) {
+        // First message
+        System.out.println("I am Happy!");
+        System.out.println("Finally starting to code");
+        System.out.println("How ya like me now!!!");
+    }
+}
