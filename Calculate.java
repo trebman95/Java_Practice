cbe6560f978d05d455d488b66b5dda10d432fc2b
trebman95 <trebman91@hotmail.com>
2024-11-18 09:30:52 -0500

    Calculate

---
+++ Calculate.java
@@ -0,0 +1,10 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int a = 5, b = 12, c = 25;
+        float myNum = c / (a + b);
+        System.out.println(a * b);
+        System.err.println(a + b);
+        System.err.println(myNum);
+    }
+
+}
