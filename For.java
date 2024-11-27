000091b44adf574858bf71c647c812a5791a07a3
trebman95 <trebman91@hotmail.com>
2024-11-27 16:38:51 -0500

    new

--- For.java
+++ For.java
@@ -4,11 +4,21 @@ public class For {
             System.out.println(i);
         }
         anotherOne(new String[] {});
+        nested(new String[] {});
     }
 
     public static void anotherOne(String[] args) {
         for (int i = 0; i < 5; i = i + 2) {
             System.out.println(i);
+        }
+    }
+
+    public static void nested(String[] args) {
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Outer: " + i);
+            for (int j = 1; j <= 3; j++) {
+                System.out.println("Inner: " + j);
+            }
         }
     }
 }
